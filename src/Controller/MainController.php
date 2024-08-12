99ec0ebdbb514e58aba29f2346fadbc175301f87
Nafees Iqbal <iqbalnafee@gmail.com>
2024-08-12 14:22:37 +0200

    twig added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class  MainController
+class  MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage() : Response{
-        return new Response('<p>Hello World!</p>');
+        return $this->render('main/homepage.html.twig');
     }
 }
