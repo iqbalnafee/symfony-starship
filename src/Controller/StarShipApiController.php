ab0d55f33f5031fc90ac1a6e1d228e0696eede46
Nafees Iqbal <iqbalnafee@gmail.com>
2024-08-12 18:32:33 +0200

    serializer added

--- src/Controller/StarShipApiController.php
+++ src/Controller/StarShipApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\model\Starship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,23 +12,12 @@ class StarShipApiController extends AbstractController
     #[Route('/api/starships', name: 'starships')]
     public function getJsonValue(): Response
     {
-        $starships = [[
-            'name' => 'USS',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'taken over by q'
-        ], [
-            'name' => 'ZZZ',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'taken over by q'
-        ], [
-            'name' => 'PPP',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'taken over by q'
-        ]];
 
-        return $this -> json($starships);
+        $starships = [new Starship(1, 'USS', 'Garden', 'kkk', 'qqq'),
+            new Starship(2, 'OOO', 'Garden', 'kkk', 'qqq'),
+            new Starship(3, 'QQQ', 'Garden', 'kkk', 'qqq')];
+
+
+        return $this->json($starships);
     }
 }
