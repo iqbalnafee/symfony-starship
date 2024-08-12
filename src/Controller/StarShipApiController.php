d72131b48bbcfa7b9c1e975fe37e5174ea6e6d0b
Nafees Iqbal <iqbalnafee@gmail.com>
2024-08-12 18:47:09 +0200

    logger added

--- src/Controller/StarShipApiController.php
+++ src/Controller/StarShipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,14 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarShipApiController extends AbstractController
 {
     #[Route('/api/starships', name: 'starships')]
-    public function getJsonValue(): Response
+    public function getJsonValue(LoggerInterface $logger): Response
     {
 
+        //dd($logger); // dd stands for dump and die
         $starships = [new Starship(1, 'USS', 'Garden', 'kkk', 'qqq'),
             new Starship(2, 'OOO', 'Garden', 'kkk', 'qqq'),
             new Starship(3, 'QQQ', 'Garden', 'kkk', 'qqq')];
 
-
+        $logger->info('log recorded');
         return $this->json($starships);
     }
 }
