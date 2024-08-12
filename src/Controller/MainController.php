1f9af14868edef5c537369855546559ca8567afc
Nafees Iqbal <iqbalnafee@gmail.com>
2024-08-12 13:30:26 +0200

    controller created

---
+++ src/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class  MainController
+{
+    #[Route('/')]
+    public function homepage() : Response{
+        return new Response('<p>Hello World!</p>');
+    }
+}
